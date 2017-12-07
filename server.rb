3435933f51d00895e3aa61399b958f1c2e97b162
Visnu Pitiyanuvath <visnup@gmail.com>
2017-12-06 23:31:48 -0800

    barely works

---
+++ server.rb
@@ -0,0 +1,12 @@
+require 'webrick'
+
+root = File.expand_path '.'
+server = WEBrick::HTTPServer.new :Port => 8000, :DocumentRoot => root
+trap 'INT' do server.shutdown end
+
+server.mount_proc '/press' do |req, res|
+  system('osascript', 'keystroke.scpt', req.query['key'])
+  res.body = 'ok'
+end
+
+server.start
