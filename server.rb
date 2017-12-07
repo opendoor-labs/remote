565de6f0073f1348c289682433025ee4ac693b3b
Visnu Pitiyanuvath <visnup@gmail.com>
2017-12-07 00:06:46 -0800

    actually works

--- server.rb
+++ server.rb
@@ -4,7 +4,7 @@ root = File.expand_path '.'
 server = WEBrick::HTTPServer.new :Port => 8000, :DocumentRoot => root
 trap 'INT' do server.shutdown end
 
-server.mount_proc '/press' do |req, res|
+server.mount_proc '/stroke' do |req, res|
   system('osascript', 'keystroke.scpt', req.query['key'])
   res.body = 'ok'
 end
