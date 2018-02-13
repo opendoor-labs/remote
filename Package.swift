35194c3e5464c28dc8133144cb7c8f9f4a5c4e1f
Visnu Pitiyanuvath <visnup@gmail.com>
2018-02-13 01:43:10 -0800

    build with "TLS"
    we don't really even care about SSL, but vapor needs to link against it.
    so, let it link against openssl then just mess it all up and point it
    against system SSL libraries. this will likely totally break if we were
    using any SSL functionality, but we're not.

--- Package.swift
+++ Package.swift
@@ -8,6 +8,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/vapor/ctls.git", from: "1.1.3"),
         .package(url: "https://github.com/vapor/vapor.git", from: "2.4.4")
     ],
     targets: [
@@ -15,6 +16,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "remote",
-            dependencies: ["Vapor"]),
+            dependencies: ["CTLS", "Vapor"]),
     ]
 )
--- README.md
+++ README.md
@@ -11,3 +11,11 @@ Solution
 --------
 This service is a web server which serves a web page with arrow and keyboard controls. It's intended that you run it on the
 machine presenting the slides. It will then relay keyboard events to the local machine, controling the running presentation.
+
+Building
+--------
+```
+swift build -Xswiftc -I/usr/local/opt/openssl/include -Xswiftc -static-stdlib -Xlinker -L/usr/local/opt/openssl/lib
+install_name_tool -change /usr/local/opt/openssl/lib/libcrypto.1.0.0.dylib /usr/lib/libcrypto.dylib .build/debug/remote
+install_name_tool -change /usr/local/opt/openssl/lib/libssl.1.0.0.dylib /usr/lib/libssl.dylib .build/debug/remote
+```
